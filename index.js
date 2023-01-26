500cfd9077f9ab2db633cebdacacb6bdabed47bf
buhtig-sudo <reservs@inbox.ru>
2023-01-26 11:43:32 +0000

    1

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
     },
   },
-});
-
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    const notes = await sequelize.query("SELECT * FROM notes", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(notes);
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
   }
-};
+);
 
-main();
+app.get("/api/notes", async (req, res) => {
+  const notes = await Note.findAll();
+  res.json(notes);
+});
+app.post("/api/notes", async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
